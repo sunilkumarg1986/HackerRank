caa76ceaa72c0bd549407770c1c892bc8817282d
sunilkumarg1986 <sunil.baijnath1986@gmail.com>
2017-06-19 10:03:51 -0500

    select and group by clause
    Advanced SQL for Group by and count, order by clause

---
+++ SQL/Advance_Select/The_PADS/BigSortingUnsorted.sql
@@ -0,0 +1,22 @@
+1. Question
+Generate the following two result sets:
+
+Query an alphabetically ordered list of all names in OCCUPATIONS, immediately followed by the first letter of each profession as a parenthetical (i.e.: enclosed in parentheses). For example: AnActorName(A), ADoctorName(D), AProfessorName(P), and ASingerName(S).
+Query the number of ocurrences of each occupation in OCCUPATIONS. Sort the occurrences in ascending order, and output them in the following format: 
+
+There are a total of [occupation_count] [occupation]s.
+where [occupation_count] is the number of occurrences of an occupation in OCCUPATIONS and [occupation] is the lowercase occupation name. If more than one Occupation has the same [occupation_count], they should be ordered alphabetically.
+
+Note: There will be at least two entries in the table for each type of occupation.
+
+
+Answer:
+
+select concat(name,
+case when occupation='Doctor' then '(D)' 
+when occupation='Actor' then '(A)'
+when occupation='Singer' then '(S)'
+when occupation='Professor' then '(P)'end)
+from OCCUPATIONS order by name;
+
+select 'There are a total of ',count(occupation), concat((LOWER(occupation)),'s.') from occupations group by occupation order by count(occupation) asc,occupation asc;s
